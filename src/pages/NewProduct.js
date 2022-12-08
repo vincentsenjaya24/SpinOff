34b8d61aa3db1b99e56c7258f0697c01b1137078
Raihan JP <raihan.jana@ui.ac.id>
2022-12-08 23:57:19 +0700

    Second Commit

--- src/pages/NewProduct.js
+++ src/pages/NewProduct.js
@@ -43,8 +43,8 @@ function NewProduct() {
     function showWidget() {
         const widget = window.cloudinary.createUploadWidget(
             {
-                cloudName: "learn-code-10",
-                uploadPreset: "dcizdwph",
+                cloudName: "dqzpjdsrq",
+                uploadPreset: "q4avlpsd",
             },
             (error, result) => {
                 if (!error && result.event === "success") {
